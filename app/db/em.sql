1c57b7f686b7013d4bdf576e5e09e2545782bd2f
Franko Fišter <ff1574@rit.edu>
2024-10-07 17:46:34 +0200

    Updating database schema

--- app/db/em.sql
+++ app/db/em.sql
@@ -38,19 +38,20 @@ CREATE TABLE IF NOT EXISTS `attendee` (
   PRIMARY KEY (`attendee_id`),
   KEY `idx_fk_role_id` (`role_id`),
   CONSTRAINT `fk_attendee_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)
+  ON DELETE RESTRICT
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 LOCK TABLES `attendee` WRITE;
 INSERT INTO `attendee` VALUES 
-(1, 'Kristina', 'Marasović', 'kxmzgr@rit.edu', 'kmu', '$2y$10$/TGAngySy2s117SvEBhQzuw5V2bVroaNbwuzgquvZ2MuhoWaD8HyS', 2),
-(2, 'Franko', 'Fišter', 'ff1574@rit.edu', 'ff1574', '$2y$10$pdb9ubrwk26x8ldH06VP126XyAMm9D2F3O7XY8IkIsNW2g.DdsRs.', 1),
-(3, 'John', 'Doe', 'john.doe@example.com', 'johndoe', '$2y$10$OgRa98PuV1AvwF9FZDBmhu0UOj0UqzMbWhO.wfsR.Z5gNWssg9Ty.', 2), 
-(4, 'Jane', 'Doe', 'jane.doe@example.com', 'janedoe', '$2y$10$0T8yyjPpytgHQrAeh0eyA.MJ6uPm2XnUHQMZQy9kTLfZnHRGgXFu6', 2),
-(5, 'Emily', 'Smith', 'emily.smith@example.com', 'emsmith', '$2y$10$FfueLuoO6VC2XlP10c0cu.JpsYJxsS/NH22gnCZXGy6M8QjvtydAy', 2),
-(6, 'Michael', 'Johnson', 'michael.johnson@example.com', 'mjohnson', '$2y$10$GhTVlQ5t/sMJ4g9h38lZhOS/R5o9zqCZyb/NpOz38B11DoxuFyfWq', 2),
-(7, 'Anna', 'Taylor', 'anna.taylor@example.com', 'annat', '$2y$10$VpXv3Fjso7B4Kov0jhMIW.VRNXPXsQosGeBb.lW8YZzqJ6fRQoxcW', 2),
-(8, 'Robert', 'Brown', 'robert.brown@example.com', 'rbrown', '$2y$10$TpzOb6ogw5k6GgcbN6PL0uOVNHZWORUfsnItTOdLfT5hszOxDZTgG', 2),
-(9, 'Linda', 'White', 'linda.white@example.com', 'lwhite', '$2y$10$LyfT/OZ5hljhG9vcjOw1Ru5jsVsSFFUcbjWemGDkVmvLx6fGO9y9.', 2);
+(1, 'Kristina', 'Marasović', 'kxmzgr@rit.edu', 'kmarasovic', '1234', 1),
+(2, 'Franko', 'Fišter', 'ff1574@rit.edu', 'franko', 'admin', 1),
+(3, 'John', 'Doe', 'john.doe@example.com', 'john', '4321', 2), 
+(4, 'Jane', 'Doe', 'jane.doe@example.com', 'jane', '4321', 2),
+(5, 'Emily', 'Smith', 'emily.smith@example.com', 'emsmith', 'em', 2),
+(6, 'Michael', 'Johnson', 'michael.johnson@example.com', 'mjohnson', 'johnny', 2),
+(7, 'Anna', 'Taylor', 'anna.taylor@example.com', 'annat', 'anna', 2),
+(8, 'Robert', 'Brown', 'robert.brown@example.com', 'rbrown', 'robby', 2),
+(9, 'Linda', 'White', 'linda.white@example.com', 'lwhite', 'walter123', 2);
 UNLOCK TABLES;
 
 --
@@ -90,6 +91,7 @@ CREATE TABLE IF NOT EXISTS `event` (
   UNIQUE KEY `unique_name` (`name`),
   KEY `idx_fk_venue_id` (`venue_id`),
   CONSTRAINT `fk_event_venue` FOREIGN KEY (`venue_id`) REFERENCES `venue` (`venue_id`)
+  ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 LOCK TABLES `event` WRITE;
@@ -113,8 +115,10 @@ CREATE TABLE IF NOT EXISTS `attendee_event` (
   PRIMARY KEY (`attendee_id`,`event_id`),
   KEY `idx_fk_attendee_id` (`attendee_id`),
   KEY `idx_fk_event_id` (`event_id`),
-  CONSTRAINT `fk_attendee_event_attendee` FOREIGN KEY (`attendee_id`) REFERENCES `attendee` (`attendee_id`),
+  CONSTRAINT `fk_attendee_event_attendee` FOREIGN KEY (`attendee_id`) REFERENCES `attendee` (`attendee_id`)
+  ON DELETE CASCADE,
   CONSTRAINT `fk_attendee_event_event` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`)
+  ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 LOCK TABLES `attendee_event` WRITE;
